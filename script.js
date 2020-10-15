eb3511d0ebd3e9cba7c70e7124a9996c8a472cca
Luk1Pr0 <lpytel16@gmail.com>
2020-10-15 20:20:08 +0200

    Added functionality to prev and next buttons

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector("#song-image");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music list
+const songs = [
+    {
+        name: "song-1",
+        displayName: "Bubbletea",
+        artist: "Quebonafide"
+    },
+    {
+        name: "song-2",
+        displayName: "SZUBIENICEPESTCYDYBRON",
+        artist: "Quebonafide"
+    },
+    {
+        name: "song-3",
+        displayName: "Stary dobry ja",
+        artist: "Reto"
+    },
+    {
+        name: "song-4",
+        displayName: "Pomarancze",
+        artist: "Tymek"
+    }
+];
 
 // Check if music is playing
 let isPlaying = false;
@@ -20,7 +47,47 @@ function pauseSong() {
     music.pause();
 }
 
-// Event listeners
+// Play or pause function
+function playOrPause() {
+    isPlaying ? pauseSong() : playSong();
+}
 
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current song
+let songIndex = 0;
+
+// Play next song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Play previous song
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Event listeners
 // Play or pause event listener
-playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener("click", playOrPause);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// On load - select first song
+loadSong(songs[songIndex]);
