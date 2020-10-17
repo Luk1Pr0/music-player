1daa1bfc08a84f5c22270229a57d9459bc096e94
Luk1Pr0 <lpytel16@gmail.com>
2020-10-17 17:09:03 +0200

    Added functionality to duration bar, and to play next song once current is ended

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector("#song-image");
 const title = document.querySelector("#title");
 const artist = document.querySelector("#artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeElement = document.getElementById("current-time");
+const durationElement = document.getElementById("duration");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -83,11 +87,48 @@ function prevSong() {
     playSong();
 }
 
+// On load - select first song
+loadSong(songs[songIndex]);
+
+// Update progress bar & time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const {duration, currentTime} = e.srcElement;
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        // Calculate display duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration element to avoid NaN
+        if (durationSeconds) {
+            durationElement.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeElement.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+}
+
+// Set progress bar
+function setProgressBar(e) {
+    const barWidth = this.clientWidth;
+    const positionX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (positionX / barWidth) * duration;
+}
+
 // Event listeners
-// Play or pause event listener
 playBtn.addEventListener("click", playOrPause);
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
-
-// On load - select first song
-loadSong(songs[songIndex]);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
+music.addEventListener("ended", nextSong);
